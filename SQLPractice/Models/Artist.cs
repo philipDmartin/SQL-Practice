35b296b9866dfb6d06f4ec164636ce253cd56b04
philipDmartin <philipnathan788@gmail.com>
2020-08-26 18:33:46 -0500

    first commit

---
+++ SQLPractice/Models/Artist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQLPractice.Models
+{
+    public class Artist
+   
+    {
+        public int Id { get; set; }
+        public int albumId { get; set; }
+        public string Name { get; set; }
+    }
+  
+}
